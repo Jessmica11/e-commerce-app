c24fff502842914f01e2536854e6f863c7241a8a
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2023-12-30 15:01:24 -0500

    forgot to save index.js, now added

--- models/index.js
+++ models/index.js
@@ -5,12 +5,16 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+// needs to relate by category_id from Product.js
 
 // Categories have many Products
+// needs to relate by category_id from Product.js
 
 // Products belongToMany Tags (through ProductTag)
+// needs to relate by product_id via joining of two tables as new table ProductTag (?)
 
 // Tags belongToMany Products (through ProductTag)
+// needs to relate by tag_id via joining of two tables as new table ProductTag (?)
 
 module.exports = {
   Product,
