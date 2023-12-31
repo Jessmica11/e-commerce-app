58fc10c0cb71ad1da1e717b623ccac770c334952
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2023-12-31 11:48:15 -0500

    added relational code for models

--- models/index.js
+++ models/index.js
@@ -6,15 +6,26 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 // needs to relate by category_id from Product.js
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
 // needs to relate by category_id from Product.js
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
 // needs to relate by product_id via joining of two tables as new table ProductTag (?)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
 // needs to relate by tag_id via joining of two tables as new table ProductTag (?)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
