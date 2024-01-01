c6a4389d5050fd6acea905936af5529f3ac3658c
Jessmica11 <116199836+Jessmica11@users.noreply.github.com>
2024-01-01 15:12:50 -0500

    fixed a mistake in sequelize code

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-const { sequelize } = require('./models');
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// middleware section
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// this is for the routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
